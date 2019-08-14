b0392bc7b53700db04f6812ab69aedc42e551aa8
max-student@outlook.com <max-student@outlook.com>
2019-08-14 16:22:02 -0400

    Added javascript

---
+++ js/index_name.js
@@ -0,0 +1,7 @@
+console.log("Connected to the js file");
+
+function log() {
+    var firstname = document.getElementById("firstname").value;
+    var lastname = document.getElementById("lastname").value;
+    console.log(firstname, lastname);
+}
